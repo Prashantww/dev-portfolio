0c8dc526d3c2e995c5f27cff33812dfa9e30a3d4
Prashant Warghude <prashant@Prashants-MacBook-Air.local>
2026-06-14 01:08:01 +0530

    updated project Images

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,25 +1,25 @@
 export const projects = [
   {
     title: "HackUnited Hero Page",
-    image: "/heroImages/img3.jpg",
+    image: "/heroImages/HackUnited.png",
     tags: ["UI Design", "Next JS"],
   },
 
   {
-    title: "Crypto Dashboard",
-    image: "/heroImages/img1.jpg",
-    tags: ["React", "GSAP"],
+    title: "Portfolio Website",
+    image: "/heroImages/TejasPortfolio.png",
+    tags: ["UI Design", "React JS"],
   },
 
   {
-    title: "Portfolio Website",
-    image: "/heroImages/img2.jpg",
-    tags: ["Frontend"],
+    title: "Crypto Dashboard",
+    image: "/heroImages/SonicExchange.png",
+    tags: ["React JS", "GSAP", "UI Design"],
   },
 
   {
     title: "Lottery Platform",
-    image: "/heroImages/img4.jpg",
-    tags: ["UI Design"],
+    image: "/heroImages/Web3.png",
+    tags: ["UI Design", "HTML & CSS"],
   },
 ];
